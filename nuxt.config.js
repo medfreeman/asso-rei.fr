1e4ea6e7aa1d5989719138288f12d54551e19194
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-28 10:54:21 +0200

    add(app): add `vue-flex` configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   build: {
-    vendor: ["framework7", "framework7-vue"]
+    vendor: ["framework7", "framework7-vue", "vue-flex"]
   },
   // include framework 7 css
-  css: ["framework7/dist/css/framework7.css"],
+  css: ["framework7/dist/css/framework7.css", "vue-flex/dist/vue-flex.css"],
   // include framework 7 & vue bindings on startup
-  plugins: [{ src: "~plugins/f7.js", ssr: false }]
+  plugins: [
+    { src: "~plugins/f7.js", ssr: false },
+    { src: "~plugins/flex.js", ssr: false }
+  ]
 };
---
+++ plugins/flex.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import VueFlex from "vue-flex";
+Vue.use(VueFlex);
