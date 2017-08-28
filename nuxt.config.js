30936cd612ccbe997a6c2da13d2b69554541f81d
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-28 13:57:30 +0200

    fix(app): change `vue-flex` configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,5 +5,5 @@ module.exports = {
   // include framework 7 css
   css: ["vue-flex/dist/vue-flex.css"],
   // include framework 7 & vue bindings on startup
-  plugins: [{ src: "~plugins/flex.js", ssr: false }]
+  plugins: ["~plugins/flex"]
 };
--- plugins/flex.js
+++ plugins/flex.js
@@ -1,3 +1,4 @@
 import Vue from "vue";
-import VueFlex from "vue-flex";
+import VueFlex from "vue-flex/dist/vue-flex.common.js"; // imports the ESM module by default
+
 Vue.use(VueFlex);
