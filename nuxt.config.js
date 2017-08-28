02acbfcb9cb4e05645a989add5733b3a3ee13bde
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-28 15:49:45 +0200

    fix(app): fix eslint, stylelint and postcss config
    Add eslint and stylelint to webpack process
    Add `postcss-reporter`, `eslint-loader`, `eslint-friendly-formatter`, `stylelint-config-standard` and `stylelint-webpack-plugin` devDependency
    Remove jsx files from prettier config
    Add css prettier to precommit hook
    Specify `node` and `browser` environments in eslint config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,26 @@
+var StyleLintPlugin = require("stylelint-webpack-plugin");
+
 module.exports = {
   build: {
     vendor: ["vue-flex"],
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/,
+          options: {
+            formatter: require("eslint-friendly-formatter")
+          }
+        });
+      }
+    },
+    plugins: [
+      new StyleLintPlugin({
+        files: "**/*.css"
+      })
+    ],
     postcss: [
       require("postcss-import")(),
       require("postcss-plugin-context")({
@@ -8,7 +28,8 @@ module.exports = {
           browserslist: "last 3 versions"
         })
       }),
-      require("postcss-cssnext")()
+      require("postcss-cssnext")(),
+      require("postcss-reporter")()
     ]
   },
   css: ["~/assets/css/main.global.css", "vue-flex/dist/vue-flex.css"],
