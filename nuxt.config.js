07683b94341c17ef7308248ca6ced1b503b9c632
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-28 14:12:59 +0200

    fix(app): remove unused comments

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ module.exports = {
   build: {
     vendor: ["vue-flex"]
   },
-  // include framework 7 css
   css: ["vue-flex/dist/vue-flex.css"],
-  // include framework 7 & vue bindings on startup
   plugins: ["~plugins/flex"]
 };
