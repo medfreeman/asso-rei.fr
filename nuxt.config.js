00274bde84510641cdbe549b753cbb56d10da9d4
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-28 13:54:34 +0200

    change(app): remove framework7
    Not compatible with ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   build: {
-    vendor: ["framework7", "framework7-vue", "vue-flex"]
+    vendor: ["vue-flex"]
   },
   // include framework 7 css
-  css: ["framework7/dist/css/framework7.css", "vue-flex/dist/vue-flex.css"],
+  css: ["vue-flex/dist/vue-flex.css"],
   // include framework 7 & vue bindings on startup
-  plugins: [
-    { src: "~plugins/f7.js", ssr: false },
-    { src: "~plugins/flex.js", ssr: false }
-  ]
+  plugins: [{ src: "~plugins/flex.js", ssr: false }]
 };
--- plugins/f7.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import "framework7";
-import Framework7Vue from "framework7-vue";
-Vue.use(Framework7Vue);
