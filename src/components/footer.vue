92288095fcdefdb4305daf45907179ebb4eb6a05
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-31 01:53:49 +0200

    change(content): adapt footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,17 +1,12 @@
 <template>
   <v-footer>
     <v-spacer/>
-    <span>&copy; {{year}}</span>
+    <span>Made with <i class="fa fa-heart"></i> using <a href="https://nuxtjs.org/" class="secondary" rel="noreferrer noopener" target="_blank">nuxt.js</a>, <a href="https://vuejs.org/" class="secondary" rel="noreferrer noopener" target="_blank">vue.js</a> & <a href="https://www.contentful.com/" class="secondary" rel="noreferrer noopener" target="_blank">contentful</a></span>
     <v-spacer/>
   </v-footer>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      year: new Date().getFullYear()
-    }
-  }
 }
 </script>
