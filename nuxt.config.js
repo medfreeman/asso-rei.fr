d33a77388896ebcd4e3083d34bcc88f7bc29aee5
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-31 11:24:14 +0200

    fix(config): removeprotocol from google fonts css url

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         rel: "stylesheet",
         href:
-          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+          "//fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
       }
     ]
   },
